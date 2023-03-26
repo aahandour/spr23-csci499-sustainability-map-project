c7b7c8cf51450a90a4b3908e2f331e5dae77a8a3
aahandour <56909131+aahandour@users.noreply.github.com>
2023-03-26 16:18:48 -0400

    Add files via upload
    Average stars (still need to get it to show up in infowindow, outside of reviews component)

--- userReviewsPage.jsx
+++ userReviewsPage.jsx
@@ -3,19 +3,40 @@ import React, { useState, useEffect } from 'react';
 const UserReviewsPage = ({targetStoreId, reviews, toggleUserReviews, setToggleUserReviews}) => {
 
     let [matchingReviews, setMatchingReviews] = useState([]);
+    let [avgStars, setAvgStars] = useState(0);
 
     useEffect(() => { //when clicked on store id changes, change displayed reviews
         let tempArray = [];
         
         for (let i = 0; i < reviews.length; i++) {
             if (targetStoreId == reviews[i].place_id) {
+
                 tempArray.push(reviews[i]);
             }
         }
+
         setMatchingReviews(tempArray);
         console.log(tempArray);
 
     }, [targetStoreId])
+
+    useEffect(() => {
+        let tempAvg = 0;
+
+        if (matchingReviews.length > 0) {
+
+            for (let i = 0; i < matchingReviews.length; i++) {
+
+                tempAvg+=matchingReviews[i].stars;
+            }
+
+            tempAvg = tempAvg/matchingReviews.length;
+        }
+
+        setAvgStars(tempAvg);
+        console.log(tempAvg);
+
+    }, [matchingReviews])
 
 
     if (toggleUserReviews == false) {
@@ -27,6 +48,7 @@ const UserReviewsPage = ({targetStoreId, reviews, toggleUserReviews, setToggleUs
             return (
             <div className = "user-reviews">
                     <p>{matchingReviews[0].name}</p>
+                    <p>{avgStars} Stars Average Community Ranking</p>
                     <input type="text"></input>
                     {matchingReviews.map(e => <div className = "review-box"><p>{e.stars} out of 6 Stars</p><p>{e.body}</p></div>)}
             </div>

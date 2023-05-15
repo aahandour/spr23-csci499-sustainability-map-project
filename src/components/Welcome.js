d8e44805360c2e5c9dc6edd3d8717023f5148b5c
Jose Florencio <fcjose2000@gmail.com>
2023-05-15 15:23:53 -0400

    fixed delete function

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 
 class Welcome extends Component {
     render(){
-        return <h1>Welcome to sustainability and etchical store map</h1>
+        return <h1>Nearby Clothing Store Sustainability Map</h1>
     }
 }
 
